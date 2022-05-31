d28bc302ae5f453b24d8950fa8bde80f5dd8f97b
Qiang Rui Zhang cheng <qiangruizhangcheng.com>
2022-05-31 11:57:36 +0200

    Utilizamos mysqli Orientado a objetos

---
+++ src/delete.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start(); 
+
+if(!isset($_SESSION['logged'])) {
+	header('Location: login.php');
+}
+
+//including the database connection file
+include("config.php");
+
+//getting id of the data from url
+$id = $_GET['id'];
+
+//deleting the row from table
+$result= $mysqli->query("DELETE FROM products WHERE id=$id");
+
+//redirecting to the display page (view.php in our case)
+header("Location:view.php");
+?>
+
